8c7ec0bdbae5e6d21def3940b4c9ba210f1b0c7f
lu.gmuer@gmail.com <lu.gmuer@gmail.com>
2014-09-30 21:12:29 +0200

    db

---
+++ SQLDatabaseProj/SQLDatabaseProj/Airport.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE TABLE [dbo].[Airport] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (MAX) NULL, 
+    CONSTRAINT [PK_Airport] PRIMARY KEY ([Id])
+);
+
+
---
+++ SQLDatabaseProj/SQLDatabaseProj/Flight.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Flight]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(MAX) NULL, 
+    [Date] DATETIME NULL, 
+    [Start] VARCHAR(MAX) NULL, 
+    [End] VARCHAR(MAX) NULL
+)
