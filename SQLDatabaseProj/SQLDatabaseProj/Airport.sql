2ad5f560922fb1a1d4241f9984c64e6c6ece8b27
lu.gmuer@gmail.com <lu.gmuer@gmail.com>
2014-11-10 20:30:51 +0100

    init

--- SQLDatabaseProj/SQLDatabaseProj/Airport.sql
+++
@@ -1,9 +0,0 @@
-﻿
-CREATE TABLE [dbo].[Airport] (
-    [Id]   INT           IDENTITY (1, 1) NOT NULL,
-    [Name] VARCHAR (MAX) NOT NULL, 
-    [FsCode] VARCHAR(MAX) NOT NULL, 
-    CONSTRAINT [PK_Airport] PRIMARY KEY ([Id])
-);
-
-
--- SQLDatabaseProj/SQLDatabaseProj/Flightmembers.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Flightmembers]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [FlightId] INT NOT NULL, 
-    [UserId] INT NOT NULL,
-    CONSTRAINT [FK_Member_ToFlight] FOREIGN KEY ([FlightId]) REFERENCES [Flight]([Id]),
-    CONSTRAINT [FK_Member_ToAirport] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
-)
